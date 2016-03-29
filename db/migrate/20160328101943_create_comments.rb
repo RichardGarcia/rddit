5708684ad9125ae808f675893f50984688601429
richardgarcia <richardgarciaph@gmail.com>
2016-03-29 23:46:43 +1300

    add 'name' in users dbase

--- db/migrate/20160328101943_create_comments.rb
+++ db/migrate/20160328101943_create_comments.rb
@@ -4,7 +4,6 @@ class CreateComments < ActiveRecord::Migration
       t.integer :link_id
       t.text :body
       t.references :user, index: true, foreign_key: true
-
       t.timestamps null: false
     end
     add_index :comments, :link_id
